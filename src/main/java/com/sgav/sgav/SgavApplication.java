aa712450fb27fc8431f6e2b150ac0f05242dd02d
fedeRomer <60818032+fedeRomer@users.noreply.github.com>
2021-07-01 13:00:59 -0300

    Initial commit

---
+++ src/main/java/com/sgav/sgav/SgavApplication.java
@@ -0,0 +1,13 @@
+package com.sgav.sgav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgavApplication.class, args);
+	}
+
+}
