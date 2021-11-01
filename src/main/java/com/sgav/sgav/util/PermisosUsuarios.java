a94ee4cf9c3124bfd591e0c5b2af5a030157e3d9
fedeRomer <60818032+fedeRomer@users.noreply.github.com>
2021-10-31 23:02:27 -0300

    Update PermisosUsuarios.java

--- src/main/java/com/sgav/sgav/util/PermisosUsuarios.java
+++ src/main/java/com/sgav/sgav/util/PermisosUsuarios.java
@@ -2,7 +2,8 @@ package com.sgav.sgav.util;
 
 public class PermisosUsuarios {
 
-    public static boolean checkUserPermissions(String type){
+    public static boolean checkUserPermissions(String typeOfUser, String moduleToAccess){
+
 
 
 
